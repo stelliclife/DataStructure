065f8e075123bdf30cac333e223d8272644767bb
stelliclife <new11dreamer@gmail.com>
2023-01-03 23:22:33 +0900

    Add Singly linked list

---
+++ Python/SignlyLinkedList/LinkedList.py
@@ -0,0 +1,85 @@
+class Node:
+    def __init__(self, data):
+        self._data = data
+        self._next = None
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def next(self):
+        return self._next
+
+    @next.setter
+    def next(self, node):
+        self._next = node
+
+
+class LinkedList:
+    def __init__(self):
+        self._head = None
+
+    def add_node_to_end(self, value):
+        node = Node(value)
+
+        if not self._head:
+            self._head = node
+        else:
+            current_node = self._head
+            while current_node:
+                if not current_node.next:
+                    current_node.next = node
+                    break
+                current_node = current_node.next
+
+    def add_node_to_start(self, value):
+        node = Node(value)
+        node.next = self._head
+        self._head = node
+
+    def insert_node_after(self, target, value):
+        node = Node(value)
+        current_node = self._head
+
+        while current_node:
+            if current_node.data == target:
+                node.next = current_node.next
+                current_node.next = node
+                break
+            current_node = current_node.next
+
+    def delete_node_at_end(self):
+        current_node = self._head
+        next_node = current_node.next
+
+        if not (current_node or next_node):
+            return current_node
+
+        while next_node:
+            if not next_node.next:
+                current_node.next = None
+                break
+            current_node = next_node
+            next_node = next_node.next
+
+        return next_node
+
+    def delete_node_at_start(self):
+        current_node = self._head
+        self._head = self._head.next
+
+        return current_node
+
+    def delete_node_after(self, value):
+        current_node = self._head
+        target_node = None
+
+        while current_node:
+            if current_node.data == value and current_node.next is not None:
+                target_node = current_node.next
+                current_node.next = target_node.next
+                break
+            current_node = current_node.next
+
+        return target_node
